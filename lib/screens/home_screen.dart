dc8c0931393706a5f60a8bbb8e38b363782270f1
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2022-03-07 23:58:54 -0600

    MovieSlider Horizontal in progress

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas_app/widgets/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  // const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Películas en cines'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined)),
+        ],
+      ),
+      body: Column(
+        children: [
+          CardSwiper(),
+          MovieSlider(),
+        ],
+      ),
+    );
+  }
+}
