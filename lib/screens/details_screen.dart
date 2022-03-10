40484cea8b9c816bd3d92b55ab4a7c0f04a4b909
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2022-03-09 19:36:20 -0600

    SliverAppBar completado

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -3,13 +3,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 class DetailsScreen extends StatelessWidget {
-  // const DetailsScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-moview';
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppBar extends StatelessWidget {
+  const _CustomAppBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Details Screen'),
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text(
+            'Movie',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://dummyimage.com/500x300/cccccc/000000'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
