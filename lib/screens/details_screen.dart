a7e60246f0ef1ebf60461f92dcb95e3ab86235d7
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2022-03-10 00:45:17 -0600

    Poster y titulo de pelicula completado

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -12,6 +12,11 @@ class DetailsScreen extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           _CustomAppBar(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              _PosterAndTitle(),
+            ]),
+          )
         ],
       ),
     );
@@ -45,6 +50,65 @@ class _CustomAppBar extends StatelessWidget {
           image: NetworkImage('https://dummyimage.com/500x300/cccccc/000000'),
           fit: BoxFit.cover,
         ),
+      ),
+    );
+  }
+}
+
+class _PosterAndTitle extends StatelessWidget {
+  const _PosterAndTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final TextTheme textTheme = Theme.of(context).textTheme;
+
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        children: [
+          ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: FadeInImage(
+                placeholder: AssetImage('assets/no-image.jpg'),
+                image: NetworkImage(
+                    'https://dummyimage.com/200x300/cccccc/000000'),
+                height: 150,
+              )),
+          SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'title',
+                style: textTheme.headline5,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+              ),
+              Text(
+                'originTitle',
+                style: textTheme.subtitle1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star_outline,
+                    size: 15,
+                    color: Colors.grey,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'voteAverage',
+                    style: textTheme.caption,
+                  )
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
