7ee38c02843499eb654b2cb0d7a5f2453d8c3800
Ricardo Hernandez <ricardo.hernandez@andonix.com>
2022-03-15 20:31:54 -0600

    mapear una peticion http completado

--- lib/providers/movie_provider.dart
+++ lib/providers/movie_provider.dart
@@ -1,12 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:peliculas_app/models/models.dart';
 
 class MoviesProvider extends ChangeNotifier {
+  String _api_key = '6a943ce652b52315a286b4f7e706d045';
+  String _baseUrl = 'api.themoviedb.org';
+  String _language = 'es-Es';
+
   MoviesProvider() {
     print('Movies Provider inicializado');
     getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '3/movie/now_playing', {
+      'api_key': _api_key,
+      'language': _language,
+      'page': '1',
+    });
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    print(nowPlayingResponse.results[0].title);
   }
 }
